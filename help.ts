a5a1dc5a56d84934b12e3678be558c167581d456
Semyon Suhorada <s.suhorada@dunice.net>
2021-07-15 09:50:28 +0300

    remove trash

--- help.ts
+++
@@ -1,7 +0,0 @@
-// request on url by axios
-// must be as controller ------------------------------
-// const axiosResponse: AxiosResponse = await axios
-//   .get('https://fish-text.ru/get')
-//   .then((res) => res);
-// return axiosResponse.data.text;
-// must be as controller ------------------------------
